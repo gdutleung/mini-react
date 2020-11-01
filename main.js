40c9dbde1a09236010fcd5a337c3de59f6d1fac7
peiran.liang <peiran.liang@zhenai.com>
2020-11-01 18:05:34 +0800

    feat: add setState and rerender

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 import { createElement, Component, render } from "./mini-react.js"
 
 class MyComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      a: 1,
+      b: 2
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState({
+      a: this.state.a + 1
+    })
+  }
   render() {
     return (
       <div>
         <h1>my component</h1>
+        <h2>a: {this.state.a.toString()}</h2>
+        <h2>b: {this.state.b.toString()}</h2>
         {this.children}
+        <button onClick={this.handleClick}>add</button>
       </div>
     )
   }
 }
 
-
-render(<div class="test">
+const template = (<div class="test">
   <div>111</div>
-  <div>222</div>
-  <div>333</div>
   <MyComponent>
     <div>my component slot1</div>
-    <div>my component slot2</div>
   </MyComponent>
-</div>, document.body)
+</div>)
+
+
+render(template, document.body)
--- mini-react.js
+++ mini-react.js
@@ -1,18 +1,41 @@
+const RENDER_TO_DOM = Symbol("render to dom")
+
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type)
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    if (name.match(/^on([\s\S]+)$/)) {
+      // 事件绑定
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute("class", value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
+    }
   }
   appendChild(component) {
-    this.root.appendChild(component.root)
+    const range = document.createRange()
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    component[RENDER_TO_DOM](range)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
@@ -21,6 +44,7 @@ export class Component {
     this.props = Object.create(null)
     this.children = []
     this._root = null
+    this._range = null
   }
   setAttribute(name, value) {
     this.props[name] = value
@@ -28,12 +52,38 @@ export class Component {
   appendChild(component) {
     this.children.push(component)
   }
-  get root() {
-    if (!this._root) {
-      // 当render出来的是一个component，那么会继续递归
-      this._root = this.render().root
+  [RENDER_TO_DOM](range) {
+    this._range = range
+    this.render()[RENDER_TO_DOM](range)
+  }
+  reRender() {
+    const oldRange = this._range
+
+    const range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.reRender()
+      return
     }
-    return this._root
+    const merge = (oldState, newState) => {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+    merge(this.state, newState)
+    this.reRender()
   }
 }
 
@@ -53,11 +103,17 @@ export function createElement(type, attributes, ...children) {
   const insertChildren = children => {
     for (let child of children) {
       if (typeof child === 'string') {
+        // 只有一个文本子节点
         child = new TextWrapper(child)
       }
+      if (child === null) {
+        continue
+      }
       if (typeof child === 'object' && child instanceof Array) {
+        // 子节点有多个
         insertChildren(child)
       } else {
+        // 只有一个元素子节点
         e.appendChild(child)
       }
     }
@@ -67,6 +123,10 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root)
+  const range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
 
